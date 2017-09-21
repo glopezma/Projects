d410c418b17fb4a6f23f6e3bdd0fa10db5b3b841
Gabriel Lopez <galopezmatthews@gmail.com>
2017-09-21 11:49:33 -0400

    Made clock work with any sized screen

--- Clock/script.js
+++ Clock/script.js
@@ -1,12 +1,18 @@
+var width;
+var height;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth - 25, windowHeight - 25);
   angleMode(DEGREES);
+
+  width = windowWidth - 25;
+  height = windowHeight - 25;
 }
 
 function draw() {
   background(0);
 
-  translate(400, 400);
+  translate(width/2, height/2);
   rotate(-90);
 
   let hr = hour();
@@ -18,14 +24,14 @@ function draw() {
   rotate(90);
   noStroke();
   textSize(25);
-  if(sec < 10){
-    sec = "0"+sec;
+  if (sec < 10) {
+    sec = "0" + sec;
   }
-  if(min < 10){
-    min = "0"+min;
+  if (min < 10) {
+    min = "0" + min;
   }
-  if(hr < 10){
-      hr = "0"+hr;
+  if (hr < 10) {
+    hr = "0" + hr;
   }
   text(hr + ":" + min + ":" + sec, -50, 50);
   pop();
@@ -34,29 +40,29 @@ function draw() {
   strokeWeight(8);
   stroke(255, 100, 150);
   let secAngle = map(sec, 0, 60, 0, 360);
-  arc(0, 0, 600, 600, 0, secAngle);
+  arc(0, 0, height * 3 / 4, height * 3 / 4, 0, secAngle);
 
   push();
   rotate(secAngle);
-  line(0, 0, 200, 0);
+  line(0, 0, height / 4, 0);
   pop();
 
   let minAngle = map(min, 0, 60, 0, 360);
   stroke(150, 100, 255);
-  arc(0, 0, 280*2, 280*2, 0, minAngle);
+  arc(0, 0, height * 3 / 4 - 20, height * 3 / 4 - 20, 0, minAngle);
 
   push();
   rotate(minAngle);
-  line(0, 0, 75*2, 0);
+  line(0, 0, height / 4 * 3 / 4, 0);
   pop();
 
   let hourAngle = map(hr % 12, 0, 12, 0, 360);
   stroke(150, 255, 100);
-  arc(0, 0, 260*2, 260*2, 0, hourAngle);
+  arc(0, 0, height * 3 / 4 - 40, height * 3 / 4 - 40, 0, hourAngle);
 
   push();
   rotate(hourAngle);
-  line(0, 0, 50*2, 0);
+  line(0, 0, height / 8, 0);
   pop();
 
   stroke(255);
