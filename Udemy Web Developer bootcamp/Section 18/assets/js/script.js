98004851ba84a9bd6231108b990afcef4702d96e
Gabriel Lopez <galopezmatthews@gmail.com>
2017-10-03 10:27:31 -0400

    Finished the To-Do List app

---
+++ Udemy Web Developer bootcamp/Section 18/assets/js/script.js
@@ -0,0 +1,21 @@
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(e) {
+  e.stopPropagation(); //stops the cross-off from the li listener
+  $(this).parent().fadeOut(333, function() {
+    $(this).remove();
+  });
+});
+
+$("input[type='text']").on("keypress", function(e) {
+  if (e.which === 13) {
+    $("ul").append("<li><span><i class=\'fa fa-trash\'></i></span> " + $(this).val() + "</li>");
+    $(this).val("");
+  }
+});
+
+$(".fa-plus").on("click", function(){
+  $("input[type='text']").fadeToggle(); 
+});
